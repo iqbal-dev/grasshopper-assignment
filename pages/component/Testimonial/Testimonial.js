a0cfe16bf57adcb0b83ec255fafa5251bd871476
iqbal-devgit config --global user.name Your <pkmdiqbal@gmail.com>
2021-08-27 15:58:13 +0600

    update

--- pages/component/Testimonial/Testimonial.js
+++ pages/component/Testimonial/Testimonial.js
@@ -17,32 +17,6 @@ const Testimonial = () => {
     slidesToShow: 2,
     slidesToScroll: 2,
     autoplay: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
   };
   return (
     <Row className="my-5">
